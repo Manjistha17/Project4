2c9f720f938d64c0922b017798fc307d3c9d90f2
Manjistha17 <141043636+Manjistha17@users.noreply.github.com>
2023-09-28 13:14:08 +0530

    Update run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,20 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="manjistha/project4"
+
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+./kubectl run project4 --image=$dockerpath --port=80 --labels app=project4
+
 
 
 # Step 3:
 # List kubernetes pods
+./kubectl get pods
+
 
 # Step 4:
 # Forward the container port to a host
-
+./kubectl port-forward project4-app 8000:80
