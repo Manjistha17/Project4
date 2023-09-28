52b930753fdc9159a1f41445663d92dc4291440c
Manjistha17 <141043636+Manjistha17@users.noreply.github.com>
2023-09-28 13:09:30 +0530

    Update upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,11 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-dockerpath="project4"
+dockerpath="manjistha/project4"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login &&\
+    docker image tag project4 $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
